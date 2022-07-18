f969a438310f08652b4a6a6aae5fe1b29b6ba3aa
elenh <yisiwings@163.com>
2022-07-18 15:41:18 +0800

    refactor: app config with dynamic and staic file

--- app.config.js
+++ app.config.js
@@ -2,41 +2,9 @@ const getEnvConfig = require('./config')
 
 const { APP_ENV } = process.env
 
-const version = '1.0.0'
-
-module.exports = {
-  expo: {
-    name: 'expo-app-typescript-starter',
-    slug: 'expo-app-typescript-starter',
-    description: 'A expo app starter!',
-    owner: 'aidolfe',
-    version,
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ['**/*'],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: 'com.elenh.expoapptypescriptstarter',
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#FFFFFF',
-      },
-      package: 'com.elenh.expoapptypescriptstarter',
-      googleServicesFile: './google-services.json',
-    },
-    web: {
-      favicon: './assets/favicon.png',
-    },
+module.exports = ({ config }) => {
+  return {
+    ...config,
     extra: getEnvConfig(APP_ENV),
-  },
+  }
 }
