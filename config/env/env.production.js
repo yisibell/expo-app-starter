3eef469bd687010c273a7fca8f6b4768ac66bdee
elenh <yisiwings@163.com>
2022-10-11 15:05:41 +0800

    refactor: upgrade expo sdk 46

--- config/env/env.production.js
+++ config/env/env.production.js
@@ -1,4 +1,7 @@
 module.exports = {
   APP_BASE_API: 'http://16.163.143.182',
   APP_MOCK_API: 'http://yapi.smaloo.com/mock/60/api',
+  eas: {
+    projectId: '80e6db1c-63be-4f13-9d29-cd07d842c5fa',
+  },
 }
